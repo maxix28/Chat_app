ec3eb82952622a3988f0f680868ccc6e478f6ad1
MaxPavliv <maxyxpavliv@gmail.com>
2023-08-31 12:10:13 +0300

    avatar random image

--- app/src/main/java/com/example/smack/CreateUserActivity.kt
+++ app/src/main/java/com/example/smack/CreateUserActivity.kt
@@ -3,13 +3,33 @@ package com.example.smack
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-
+import com.example.smack.databinding.ActivityCreateUserBinding
+import java.util.Random
+import com.example.smack.databinding.ActivityMainBinding
 class CreateUserActivity : AppCompatActivity() {
+     lateinit var  binding: ActivityCreateUserBinding
+     var userAvatar= "profileDefault"
+    var avatarColor="[0.5, 0.5, 0.5, 1]"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_user)
+        binding = ActivityCreateUserBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
     fun genereteUserAvatar(view: View){
+        val random= Random()
+        val color = random.nextInt(2)
+val avatar= random.nextInt(28)
+        println(avatar)
+        println(color)
+        if (color==0){
+            userAvatar="light$avatar"
+        }
+        else{
+            userAvatar="dark$avatar"
+        }
+        val resourseID= resources.getIdentifier(userAvatar,"drawable",packageName)
+binding.createAvatarImageView.setImageResource(resourseID)
 
     }
     fun createUserClicked(view:View){
